1e729c0aa25829ba7eb7ef94ff1c564b62056773
Banatovskaya <50458075+Banatovskaya@users.noreply.github.com>
2023-11-07 14:39:17 +0000

    changed deleting empty lines

--- index.js
+++ index.js
@@ -1,55 +1,39 @@
 const fs = require('fs');
 const http = require('http');
 
-let d
-// const a = fs.readFile('1.txt', 'utf8', (err,data)=>{
-//     if (err) throw err
-//     d = data
-// })
-
 const server = http.createServer((req, res) => {
     res.write(`<h1>${d}</h1>`)
     res.end()
-  
 })
 
 server.listen(3000, () => {
     console.log('server is running...')
-})
+});
+
 const readStream = fs.createReadStream("exp.psd", {});
 const writeStream = fs.createWriteStream('newFile.txt')
 readStream.on("data", function(chunk){ 
     let newChunkStr = '';
     let exceptionCodes = [0,1,2,3,4,5,6,7,8,9,11,12,14,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,65533];
-    // console.log('______chunk_____', );
     let line = '';
     let isFullLine = false;
     for (let i = 0; i < (chunk.length-1); i++){
         if (!exceptionCodes.includes(chunk.toString().charCodeAt(i))) {
             line = line.concat(chunk.toString().charAt(i))
             if (chunk.toString().charCodeAt(i) != 32 && chunk.toString().charCodeAt(i) != 10) {
-                isFullLine = isFullLine || true;
-            } else if (chunk.toString().charCodeAt(i) == 32){
-                isFullLine = isFullLine || false;
+                isFullLine = true;
             } 
-            if (chunk.toString().charCodeAt(i) == 10  && isFullLine == true) {
-                newChunkStr = newChunkStr.concat(line);
-                isFullLine = false;
-                line = '';    
-            } else if (chunk.toString().charCodeAt(i) == 10  && isFullLine == false){
+            if (chunk.toString().charCodeAt(i) == 10) {
+               if(isFullLine == true) {
+                    newChunkStr = newChunkStr.concat(line);
+                    isFullLine = false;
+               }
                 line = ''; 
-                isFullLine = false;
             }
-           
         }
     }
    console.log(newChunkStr);
    writeStream.write(newChunkStr)
-//    console.log('newChunk', newChunk)
-// console.log(String.fromCharCode(10))
-// console.log(String.fromCodePoint(0o1,55, 0o10125, 4181))
-// console.log('  '.charCodeAt(1))
-// console.log('Adf5фф☺7ၕ'.codePointAt(8))
 });
 
 // readStream = fs.createReadStream("newFile.txt", {});
